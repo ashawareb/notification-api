eb06b5d0592e0ee94b25f0361a9444451c73a91b
AbdUlRahman Shawareb <51769400+AbdUlRahmanShawareb@users.noreply.github.com>
2020-12-25 15:29:31 +0200

    Update NotificationSystemApplication.sql

--- Database/NotificationSystemApplication.sql
+++ Database/NotificationSystemApplication.sql
@@ -1,7 +1,11 @@
 CREATE DATABASE Notification_System;
-
+-- _________________________________
 CREATE TABLE Notification_Templates(
 	subject VARCHAR(100) NOT NULL,
 	body VARCHAR(500) NOT NULL,
 	id INT NOT NULL UNIQUE
 );
+-- _________________________________
+ALTER TABLE Notification_Templates
+    ADD language VARCHAR(100) NOT NULL;
+-- _________________________________
