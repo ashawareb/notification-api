afbe1eb78097c16d42cf143ff9ef9bc10933716e
omar-hany <62159949+omar-hany@users.noreply.github.com>
2020-12-27 15:53:17 +0200

    Update NotificationSystemApplication.sql

--- Database/NotificationSystemApplication.sql
+++ Database/NotificationSystemApplication.sql
@@ -9,3 +9,20 @@ CREATE TABLE Notification_Templates(
 ALTER TABLE Notification_Templates
     ADD language VARCHAR(100) NOT NULL;
 -- _________________________________
+create table sms(
+    sender varchar(255),
+    receiver varchar(255),
+    subject varchar(255),
+    body varchar(255),
+    language varchar(255),
+    id int not null,
+    status boolean);
+--_________________________________
+create table mail(
+    sender varchar(255),
+    receiver varchar(255),
+    subject varchar(255),
+    body varchar(255),
+    language varchar(255),
+    id int not null,
+    status boolean);
